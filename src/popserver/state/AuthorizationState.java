a7218dda24f4608b5c2a2d5ae17e8f2eded88460
Thomas RICHARD <thomas.richard.pro@gmail.com>
2014-04-01 17:41:41 +0200

    Moving timestamp generation from CommandOk to AuthorizationState.

--- src/popserver/state/AuthorizationState.java
+++ src/popserver/state/AuthorizationState.java
@@ -5,6 +5,8 @@ import poplib.service.DeliveryService;
 import poplib.state.AbstractState;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class AuthorizationState extends AbstractState {
 
@@ -15,12 +17,25 @@ public class AuthorizationState extends AbstractState {
     @Override
     public void run() {
         try {
-            CommandOk command = new CommandOk("server ready");
+            CommandOk command = new CommandOk("server ready" + getTimestamp());
             System.out.println("Command: " + command.toString());
             deliveryService.send(command);
+            System.out.println("Sent");
         } catch(IOException e) {
             //TODO
             e.printStackTrace();
         }
+    }
+
+    private String getTimestamp() {
+        String hostname;
+        try {
+            hostname = InetAddress.getLocalHost().getHostName();
+        } catch(UnknownHostException e) {
+            hostname = "localhost";
+        }
+        return "<" + Thread.currentThread().getId() + "." +
+                System.currentTimeMillis() + "@" +
+                hostname + ">";
     }
 }
